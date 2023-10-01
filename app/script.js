9bd7b18377e7069083514c6d1311acbf08e2d4f3
York <goog10216922@gmail.com>
2023-10-01 21:57:32 +0800

    Login Page Added

--- app/script.js
+++ app/script.js
@@ -1,61 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const startNumberInput = document.getElementById("start-number");
-    const endNumberInput = document.getElementById("end-number");
-    const advancedToggle = document.getElementById("advanced-toggle");
-    const advancedSettings = document.getElementById("advanced-settings");
-    const excludedNumbersInput = document.getElementById("excluded-numbers");
-    const generateButton = document.getElementById("generate-button");
-    const resultElement = document.getElementById("result");
+const users = [
+    { username: "York", password: "york0524" },
+    { username: "PublicTest", password: "PasswordForPublicTest" }
+];
 
-    const numPeopleInput = document.getElementById("num-people");
-    const oddEvenToggle = document.getElementById("odd-even-toggle");
-
-    advancedToggle.addEventListener("change", function () {
-        advancedSettings.style.display = advancedToggle.value === "on" ? "block" : "none";
-    });
-
-    generateButton.addEventListener("click", function () {
-        const start = parseInt(startNumberInput.value);
-        const end = parseInt(endNumberInput.value);
-        const excludedNumbers = excludedNumbersInput.value.split(",").map(num => parseInt(num.trim()));
-        const numPeople = parseInt(numPeopleInput.value);
-        const selectedOddEven = oddEvenToggle.value;
-
-        if (isNaN(start) || isNaN(end)) {
-            alert("請輸入有效的起始號和末號!");
-            return;
-        }
-
-        if (start > end) {
-            alert("起始號不能大於末號!");
-            return;
-        }
-
-        const availableNumbers = [];
-        for (let i = start; i <= end; i++) {
-            if (!excludedNumbers.includes(i)) {
-                if (selectedOddEven === "odd" && i % 2 === 0) {
-                    continue; // 跳過偶數號碼
-                }
-                if (selectedOddEven === "even" && i % 2 !== 0) {
-                    continue; // 跳過奇數號碼
-                }
-                availableNumbers.push(i);
-            }
-        }
-
-        if (availableNumbers.length === 0) {
-            alert("所有座號都被排除了是要抽什麼XD");
-            return;
-        }
-
-        const selectedNumbers = [];
-        for (let i = 0; i < numPeople; i++) {
-            const randomIndex = Math.floor(Math.random() * availableNumbers.length);
-            const selectedNumber = availableNumbers.splice(randomIndex, 1)[0];
-            selectedNumbers.push(selectedNumber);
-        }
-
-        resultElement.textContent = `選中的號碼是：${selectedNumbers.join(", ")} 號 恭喜!`;
-    });
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+        window.location.href = "./ruapp/index.html";
+    } else {
+        alert("錯誤的使用者名稱或密碼!");
+    }
 });
