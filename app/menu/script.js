635e44f8404238021595ac6ee612a81a053f8236
York <goog10216922@gmail.com>
2023-10-29 18:43:22 +0800

    Update script.js

--- app/menu/script.js
+++ app/menu/script.js
@@ -1,7 +1,3 @@
 document.getElementById('button1').addEventListener('click', function() {
-    window.location.href = '../ruapp/index.html'; // 跳轉至網頁1
-});
-
-document.getElementById('button2').addEventListener('click', function() {
-    window.location.href = '../../login.html';
+    window.location.href = '../ruapp/index.html';
 });
