34fbb90c0bff8b4c3053ff7add3d4e7e4d218723
York <goog10216922@gmail.com>
2024-01-22 19:10:14 +0800

    Add 'Clock' app

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 # 免費抽獎官方網站原始碼
 
 這裡是免費抽獎網站的原始碼，可供參考學習等用途使用
-## 作者
-
-- [Tomato6966](https://github.com/Tomato6966) (此網站原作者)
-- [York](https://github.com/york9675/)
 ## 問題回報
 
 如在使用此網站時遇到任何問題，請至[issues頁面](https://github.com/york9675/website/issues)回報
--- apps/menu/script.js
+++ apps/menu/script.js
@@ -1,3 +1,6 @@
 document.getElementById('button1').addEventListener('click', function() {
     window.location.href = '../ruapp/index.html';
 });
+document.getElementById('button2').addEventListener('click', function() {
+    window.location.href = '../clock/index.html';
+});
