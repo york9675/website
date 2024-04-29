f72bc4549c606ee0374c3fafb07caf1c3116a781
York <goog10216922@gmail.com>
2024-04-29 10:30:29 +0800

    add dcc

---
+++ apps/DCC/script.js
@@ -0,0 +1,56 @@
+let isWhite = false;
+let fadeOutTimeout;
+
+function setWhite() {
+    isWhite = true;
+    document.body.style.transition = '';
+    document.body.style.backgroundColor = 'white';
+}
+
+function setBlack(event) {
+    isWhite = false;
+    if (event && (event.key === 's' || event.key === 'S')) {
+        document.body.style.transition = 'background-color 1s ease-out';
+    }
+    document.body.style.backgroundColor = 'black';
+}
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'a' || event.key === 'A') {
+        setWhite();
+    } else if (event.key === 's' || event.key === 'S') {
+        setWhite();
+        clearTimeout(fadeOutTimeout);
+    }
+});
+
+document.addEventListener('keyup', function (event) {
+    if (event.key === 'a' || event.key === 'A') {
+        requestAnimationFrame(() => {
+            setBlack(event);
+        });
+    } else if (event.key === 's' || event.key === 'S') {
+        requestAnimationFrame(() => {
+            fadeOutTimeout = setTimeout(() => {
+                setBlack(event);
+            }, 0);
+        });
+    }
+});
+
+document.addEventListener('touchstart', function () {
+    setWhite();
+});
+
+document.addEventListener('touchend', function () {
+    fadeOutTimeout = setTimeout(setBlack, 1000); // Delay setting black for 1 second
+});
+
+// Cancel timeout if touch is moved or canceled
+document.addEventListener('touchmove', function () {
+    clearTimeout(fadeOutTimeout);
+});
+
+document.addEventListener('touchcancel', function () {
+    clearTimeout(fadeOutTimeout);
+});
--- apps/menu/script.js
+++ apps/menu/script.js
@@ -2,5 +2,5 @@ document.getElementById('button1').addEventListener('click', function() {
     window.location.href = '../ruapp/index.html';
 });
 document.getElementById('button2').addEventListener('click', function() {
-    window.location.href = '../clock/index.html';
+    window.location.href = '../DCC/index.html';
 });
