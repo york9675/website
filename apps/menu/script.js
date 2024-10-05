3d66c592f2c79c4e274d488cac813f7f41a1f47a
York <goog10216922@gmail.com>
2024-10-05 14:51:27 +0800

    Update script.js

--- apps/menu/script.js
+++ apps/menu/script.js
@@ -4,6 +4,9 @@ document.getElementById('button1').addEventListener('click', function() {
 document.getElementById('button2').addEventListener('click', function() {
     window.location.href = '../DCC/index.html';
 });
-document.getElementsById('button3').addEventListener('click', function() {
-    window.location.herf = '../stat/index.html';
-})
+document.getElementById('button3').addEventListener('click', function() {
+    window.location.href = '../stat/index.html';
+});
+document.getElementById('button4').addEventListener('click', function() {
+    window.location.href = '../clock/index.html';
+});
